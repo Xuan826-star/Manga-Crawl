a21c551d2587228574cb9d8d371501673f307306
Xuan826-star <xyi@ucsb.edu>
2022-08-06 11:50:32 -0700

    Complete the first version of Source_code Crawl.py

--- Source_code/Crawl.py
+++ Source_code/Crawl.py
@@ -1,6 +1,7 @@
 from urllib.error import HTTPError
 import requests
 from bs4 import BeautifulSoup as bs
+import os
 
 base='https://www.baozimh.com/'
 
@@ -37,10 +38,50 @@ def Parse_Search(res:requests.Response):
         dic['author']=info2.find('small').text.strip()
         rtls.append(dic)
     return rtls
+
 def Parse_Chapters(res:requests.Response):
-    '''Parse the chapter result. Yield the dict of chapter&href. 50 chapters per time. 
+    '''Parse the chapter result. Return the list of chapters in type of tuple (name,href).
     '''
-    pass
+    rtls=[]
+    soup=bs(res.text,'html.parser')
+    chapters=soup.find_all('a',class_='comics-chapters__item')
+    for chapter in chapters:
+        name=chapter.text
+        href=chapter.attrs['href']
+        single=(name,href)
+        rtls.append(single)
+    return rtls
+
+def Parse_Pages(res:requests.Response):
+    '''Parse the page result in one chapter. Return the list of page.
+    '''
+    rtls=[]
+    soup=bs(res.text,'html.parser')
+    sub=soup.find('ul',class_='comic-contain')
+    pages=sub.find_all('amp-img')
+    for page in pages:
+        dir={}
+        dir['width']=page.attrs['width']
+        dir['height']=page.attrs['height']
+        dir['src']=page.attrs['src']
+        rtls.append(dir)
+    return rtls
+
+def Rename(url):
+    return url.split('scomic/')[-1].replace('/','_')
+    
+def Cache(url:str,path:str):
+    '''Download the page. 可改进协程
+
+    '''
+    down_res = requests.get(url)
+    filename=Rename(url)
+    complete_path=path+filename
+    if os.path.exists(complete_path)==False:
+        with open(complete_path,'wb') as file:
+            file.write(down_res.content)
+    else:
+        print('Already exist {}'.format(complete_path))
 
 if __name__=='__main__':
     Name='我独自升级'
@@ -52,5 +93,14 @@ if __name__=='__main__':
         # print(rtv.text.find(Name))
         rtls=Parse_Search(rtv)
         # print(rtls)
+        chapter_res=Get_Response(Ses,rtls[0]['href'])
+        chapter_list=Parse_Chapters(chapter_res)
+        page_res=Get_Response(Ses,chapter_list[0][1])
+        page_list=Parse_Pages(page_res)
+        # print(page_list)
+        current_path = os.path.abspath(__file__)
+        dirname=os.path.dirname(current_path)
+        for page in page_list:
+            Cache(page['src'],dirname+'\\..\\Cache\\')
 
 
