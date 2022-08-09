1ced6fdb4760ebaca75d5d4a35776088112fdda9
Xuan826-star <xyi@ucsb.edu>
2022-08-08 18:27:06 -0700

    Apply the Crawl.py in GUI.py

--- GUI.py
+++ GUI.py
@@ -1,6 +1,8 @@
 from PySide2.QtWidgets import QApplication, QMessageBox
 from PySide2.QtUiTools import QUiLoader
+import requests
 import os
+import Crawl
 current_path = os.path.abspath(__file__)
 dirname=os.path.dirname(current_path)
 print(dirname)
@@ -12,8 +14,19 @@ class home:
         # 注意：里面的控件对象也成为窗口对象的属性了
         # 比如 self.ui.button , self.ui.textEdit
         self.ui = QUiLoader().load(dirname+'\GUI\main.ui')
+        self.ses=requests.session()
+        self.ui.search_button.clicked.connect(self.search)
+
+    def search(self):
+        name=self.ui.search_text.text()
+        param={'q':name}
+        shref='search'
+        rtv=Crawl.Get_Response(self.ses,shref,params=param)
+        if rtv!=None:
+            rtls=Crawl.Parse_Search(rtv)
+            print(rtls)
 
 app = QApplication([])
-home = home()
-home.ui.show()
+home_0 = home()
+home_0.ui.show()
 app.exec_()
