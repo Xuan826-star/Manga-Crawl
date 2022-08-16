00f4917079cb868e904242b6786c2326fcaea3c0
Xuan826-star <xyi@ucsb.edu>
2022-08-16 11:36:48 -0700

    Update

--- GUI.py
+++ GUI.py
@@ -1,10 +1,12 @@
 
-from PySide2.QtWidgets import QApplication, QMessageBox,QLabel,QWidget
+from PySide2.QtWidgets import QApplication, QMessageBox,QLabel,QWidget,QPushButton
 from PySide2.QtUiTools import QUiLoader
 from PySide2.QtCore import QRegularExpression
+from PySide2.QtGui import QPixmap
 import requests
 import os
 import Crawl
+
 
 one_re=QRegularExpression()
 one_re.setPattern('one.*')
@@ -40,37 +42,40 @@ class Home:
             print(rtls)
             self.open_result_window(rtls)
 
-class Search:
+    def clean_cache(self):
+        pass
+
+
+class Search(Home):
     def __init__(self,session,reslist):
-        self.ui = QUiLoader().load(dirname+'\GUI\search.ui')
+        ld=QUiLoader()
+        ld.addPluginPath(dirname)
+        self.ui = ld.load(dirname+'\GUI\search.ui')
+        print(ld.pluginPaths())
         self.ses=session
         self.lst=reslist
         self.ui.search_button.clicked.connect(self.search)
         onelst=self.ui.findChildren(QWidget,one_re)
         onelen=len(onelst)
         for i in range(onelen):
-            Crawl.Cache(0,reslist[i]['href'],cache_path)
+            Crawl.Cache(0,reslist[i]['src'],cache_path)
             picname=Crawl.Rename_Cover(reslist[i]['href'])
-            onelst[i].findChild(QLabel,'src_{}'.format(i+1)).setPixmap()
+            pixm=QPixmap(cache_path+picname)
+            each_cover=onelst[i].findChild(QLabel,'src_{}'.format(i+1))
+            each_cover.setPixmap(pixm)
+            each_name=onelst[i].findChild(QPushButton,'name_{}'.format(i+1))
+            each_name.setText(reslist[i]['title'])
+            each_name.setWhatsThis(reslist[i]['href'])
+            each_name.clicked.connect(self.chapter)
+            each_author=onelst[i].findChild(QLabel,'author_{}'.format(i+1))
+            each_author.setText(reslist[i]['author'])
 
-            
-            
+    def chapter(self):
+        print('clicked')
 
-    def search(self):
-        name=self.ui.search_text.text()
-        param={'q':name}
-        shref='search'
-        rtv=Crawl.Get_Response(self.ses,shref,params=param)
-        if rtv!=None:
-            rtls=Crawl.Parse_Search(rtv)
-            print(rtls)
-
-    def open_result_window(self,reslist):
-        # 实例化另外一个窗口
-        self.result = Search(self.ses,reslist)
-        # 显示新窗口
-        self.result.ui.show()
-        self.ui.close()
+class Chapter():
+    def __init__(self):
+        pass
 
 app = QApplication([])
 ses=requests.session()
